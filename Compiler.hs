b88c5b5cafd91c158ee100b0e62b0cca87515edb
Dougal Stanton <dougal@dougalstanton.net>
2009-01-18 19:07:31 +0000

    Read in and parse image map from file
    darcs-hash:20090118190731-1b04a-9f284adb72e4ebf69d9317cbfa3557ae62bf3bac

--- Compiler.hs
+++ Compiler.hs
@@ -4,10 +4,15 @@ import Control.Monad (liftM)
 import System.FilePath (takeDirectory)
 
 import Script
+import CastList
 
 import ScriptParse
+import ImageMapParse
 
 readScript :: FilePath -> IO (Script [Scene])
 readScript filename = parseScript `liftM` readFile filename
     where parseScript = rawToScenes . flip rawScript path
           path = takeDirectory filename
+
+readImageMap :: FilePath -> IO CastList
+readImageMap filename = parseImageMap `liftM` readFile filename
