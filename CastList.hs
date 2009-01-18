6113c3a95fe64e36f28d81ef3084da8c97916f37
Dougal Stanton <dougal@dougalstanton.net>
2009-01-18 18:44:09 +0000

    Make CastList type opaque
    Made some accessor functions and limited the export list.
    
    darcs-hash:20090118184409-1b04a-cf27b0c77271bb369659d860c01b35d4de988f02

--- CastList.hs
+++ CastList.hs
@@ -1,15 +1,21 @@
-module CastList where
+module CastList (CastList, addCharacter, getCoords, emptyCast) where
 
 import qualified Data.Map as M
 
 type Character = String
 type Coordinate = (Int, Int)
-type CastList = M.Map Character [Coordinate]
+newtype CastList = ImageMap { imageMap :: (M.Map Character [Coordinate]) }
+
+emptyCast :: CastList
+emptyCast = ImageMap M.empty
 
 addCharacter :: Character -> [Coordinate] -> CastList -> CastList
-addCharacter ch coords list | validInput = M.insert ch coords list
+addCharacter ch coords list | validInput = ImageMap $ M.insert ch coords $ imageMap list
                             | otherwise = list
         where validInput = validCharacter ch && validCoords coords
+
+getCoords :: Character -> CastList -> Maybe [Coordinate]
+getCoords character = M.lookup character . imageMap
 
 validCharacter = not . null
 validCoords = even . length
