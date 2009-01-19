102033f637d94622e406b72eb18d4a41be84bdd8
Dougal Stanton <dougal@dougalstanton.net>
2009-01-19 00:05:24 +0000

    Parse and include locations of characters
    The cast list is parsed from image map files,
    one for each scene, and included in the relevant scenes.
    
    darcs-hash:20090119000524-1b04a-d1b15851a7e921bc860b77b451658b66802b6011

--- Compiler.hs
+++ Compiler.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import Control.Monad (liftM)
-import System.FilePath (takeDirectory)
+import System.FilePath (takeDirectory, replaceExtension, (</>))
 
 import Script
 import CastList
@@ -16,3 +16,21 @@ readScript filename = parseScript `liftM` readFile filename
 
 readImageMap :: FilePath -> IO CastList
 readImageMap filename = parseImageMap `liftM` readFile filename
+
+stage3 script = do castlists <- mapM readImageMap (imgmaps script)
+                   return $ fmap (\scenes -> zipWith stageN scenes castlists) script
+
+    where   imageMapName = (</>) (scriptLocation script) . flip replaceExtension "map"
+            imgmaps = map (imageMapName . sceneBackground) . scriptContents
+
+actorsInPlace :: CastList -> Speech -> Cue
+actorsInPlace list speech = Cue { cueActor = fst speech
+                                , cueWords = snd speech
+                                , cueCoord = getCoords (fst speech) list
+                                }
+
+stageN :: Scene -> CastList -> Scene
+stageN scene castlist = scene { sceneDialogue = map (shift (actorsInPlace castlist)) dialogue }
+        where shift :: (b -> a) -> Either a b -> Either a b
+              shift f (Right r) = Left (f r)
+              dialogue = sceneDialogue scene
