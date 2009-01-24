90c9491e33754d0c6e5a242678a028cc11a442f8
Dougal Stanton <dougal@dougalstanton.net>
2009-01-24 16:25:15 +0000

    Clean up functions: reorder arguments and add comments
    darcs-hash:20090124162515-1b04a-a3440dfd28c9fa23959732ce55b1eed8909a4025

--- Compiler.hs
+++ Compiler.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import Control.Monad (liftM)
-import System.FilePath (takeDirectory, replaceExtension, (</>))
+import System.FilePath (takeDirectory, replaceExtension, combine)
 
 import Script
 import CastList
@@ -17,10 +17,13 @@ readScript filename = parseScript `liftM` readFile filename
 readImageMap :: FilePath -> IO CastList
 readImageMap filename = parseImageMap `liftM` readFile filename
 
+-- | Look up image maps and place details of actors'
+--   locations into the script.
+stage3 :: Script [Scene] -> IO (Script [Scene])
 stage3 script = do castlists <- mapM readImageMap (imgmaps script)
-                   return $ fmap (\scenes -> zipWith stageN scenes castlists) script
+                   return $ fmap (zipWith stageN castlists) script
 
-    where   imageMapName = (</>) (scriptLocation script) . flip replaceExtension "map"
+    where   imageMapName = combine (scriptLocation script) . flip replaceExtension "map"
             imgmaps = map (imageMapName . sceneBackground) . scriptContents
 
 actorsInPlace :: CastList -> Speech -> Cue
@@ -29,8 +32,11 @@ actorsInPlace list speech = Cue { cueActor = fst speech
                                 , cueCoord = getCoords (fst speech) list
                                 }
 
-stageN :: Scene -> CastList -> Scene
-stageN scene castlist = scene { sceneDialogue = map (shift (actorsInPlace castlist)) dialogue }
+-- | Given a complete list of cast locations for a scene
+--   replace every instance of a bare speech in the scene
+--   with the annotated equivalent.
+stageN :: CastList -> Scene -> Scene
+stageN castlist scene = scene { sceneDialogue = map (shift (actorsInPlace castlist)) dialogue }
         where shift :: (b -> a) -> Either a b -> Either a b
               shift f (Right r) = Left (f r)
               dialogue = sceneDialogue scene
