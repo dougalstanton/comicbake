ac793ce8a93e9f0bf29dc032183aacee914102f6
Dougal Stanton <dougal@dougalstanton.net>
2009-04-21 23:08:48 +0100

    Clean up formatting and imports
    Ignore-this: 63f3daa0189671d0e51e739c76e13a72
    
    darcs-hash:20090421220848-1b04a-59fefdc10d4e50a4610d81d3fdd29bd6c077ccb1

--- Script.hs
+++ Script.hs
@@ -1,28 +1,28 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Script where
 
-import Data.List (nub)
-
 type Character = String
 type Frame     = [(Int,Int)] -- box containing character
 
 
-data Script a = Script { scriptTitle    :: String
-                       , scriptCredits  :: [String]
-                       , scriptLocation :: FilePath
-                       , scriptContents :: a
-                       } deriving (Eq, Show)
+data Script a = Script
+    { scriptTitle    :: String
+    , scriptCredits  :: [String]
+    , scriptLocation :: FilePath
+    , scriptContents :: a
+    } deriving (Eq, Show)
 
 instance Functor Script where
     fmap f script = script { scriptContents = f (scriptContents script) }
 
 data Scene = Scene
-                { sceneNumber       :: Int
-                , sceneBackground   :: FilePath
-                , sceneAction       :: [Action]
-                } deriving (Eq, Show)
+    { sceneNumber     :: Int
+    , sceneBackground :: FilePath
+    , sceneAction     :: [Action]
+    } deriving (Eq, Show)
 
-data Action = Action { character :: Character
-		     , speech    :: [String]
-		     , position  :: Maybe Frame
-		     } deriving (Eq, Show)
+data Action = Action
+    { character :: Character
+    , speech    :: [String]
+    , position  :: Maybe Frame
+    } deriving (Eq, Show)
