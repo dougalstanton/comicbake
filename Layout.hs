c1026e0ca565b0c6de1d246d9f0e501579ad2e22
Dougal Stanton <dougal@dougalstanton.net>
2009-08-30 16:02:46 +0100

    Frames are placed below current frames
    Ignore-this: e31db9d4620ae3b824972204b340599
    
    darcs-hash:20090830150246-1b04a-ff59756368e8f93c496504f1208062ca6a375d52

--- Layout.hs
+++ Layout.hs
@@ -5,7 +5,9 @@ import Data.Maybe (mapMaybe)
 import Script
 
 type Dim = (Int,Int)
+type Pt = Dim
 type Order = [Int]
+
 
 x1,y1,x2,y2 :: Frame -> Int
 x1 = fst . head
@@ -15,6 +17,7 @@ y2 = y1 . drop 1
 midx f = x1 f + ((x2 f + x1 f)`div`2)
 midy f = y1 f + ((y2 f + y1 f)`div`2)
 
+
 -- A single comic panel, with speech bubbles,
 -- character outlines, etc.
 
@@ -22,13 +25,23 @@ data Panel = Panel { number     :: Int
 		   , background :: FilePath
 		   , characters :: [Frame]
 		   , bubbles    :: [Frame]
-		   }
+		   } deriving Show
 
 strSize :: [String] -> Dim
 strSize str = ((5*) $ maximum $ map length str, 10*length str)
 
 oddsweep = reverse [5..11]
 evensweep = [1..7]
+
+
+inFrames :: [Frame] -> Pt -> Bool
+inFrames fs pt = any (inLine pt) fs
+  where inFrame d f = fst d < x2 f && fst d > x1 f || snd d < y2 f && snd d > y1 f
+	-- Is this point in the same row as a frame?
+        inLine p f = snd p < y2 f && snd p > y1 f
+
+allPos :: Dim -> Bool
+allPos d = fst d > 0 && snd d > 0
 
 --
 -- Convert a scene into a panel
@@ -41,15 +54,18 @@ f s = foldr enplace base (sceneAction s)
 		    , characters = mapMaybe position $ sceneAction s
 		    , bubbles = [] }
 
-genlocs :: Dim -> Frame -> [Frame]
-genlocs d f = (midx f, midy f)
+genlocs :: Dim -> Frame -> [Dim]
+genlocs d f = [(x,y)| x <- [(x1 f - fst d)..(x2 f + fst d)], y <- [(y1 f - snd d)..(y2 f + snd d)]]
 
-getlocation :: Dim -> Action -> [Frame] -> Frame
-getlocation d a fs = head $ filter fs $ genlocs d (position a)
+getlocation :: Action -> [Frame] -> Frame
+getlocation a fs = [d', (fst d + fst d', snd d + snd d')]
+	where d' = head $ filter allPos $ filter (not . inFrames fs) $ genlocs d (maybe origin id $ position a)
+              d = strSize $ speech a
+	      origin = [(0,0),(0,0)]
 
+-- Decide best place to put this speech bubble in the
+-- provided panel, then place it there.
 enplace :: Action -> Panel -> Panel
 enplace a p = p { bubbles = loc : bubbles p }
  where loc = getlocation a (characters p ++ bubbles p)
-
-
 
