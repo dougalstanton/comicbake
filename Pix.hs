5b163fae8b9c226bb204900a62edea4c73122086
Dougal Stanton <dougal@dougalstanton.net>
2009-09-02 00:02:11 +0100

    Bubbles with tails (not directed)
    Ignore-this: 39dd22c5dec17f9f88eeb5b494f07184
    
    The speech bubbles now have tails but they're a
    bit crap --- they don't go in the right direction
    and they are always on top of the bubbles.
    
    darcs-hash:20090901230211-1b04a-8ae9219d3a4fed57abd1c50c5bccaa9c9d6f4961

--- Pix.hs
+++ Pix.hs
@@ -3,6 +3,7 @@ module Pix (Pix(..), panel2pix, writePix) where
 import Graphics.Rendering.Diagrams
 import qualified Graphics.Rendering.Diagrams.Types as DT
 import System.FilePath (combine)
+import Control.Arrow ((***))
 
 import Script hiding (position)
 import Layout
@@ -12,6 +13,8 @@ data Pix = Pix { bgImage :: FilePath
 	       , order :: Int
 	       }
 
+type Dim = (Int,Int)
+
 panel2pix panel = Pix { bgImage = background panel
                       , overlays = corner:map mkdiagram (bubbles panel)
 		      , order = number panel
@@ -20,12 +23,29 @@ panel2pix panel = Pix { bgImage = background panel
 -- Diagrams can be accurately positioned with respect to
 -- the top-left diagram, so we need something in the
 -- corner in order to place everything else accurately.
+corner :: (DT.Point, DT.Diagram)
 corner = ((0,0),empty 0 0)
 
+underlay a b = a `withSize` b ## b
+atop a b = vcatA hcenter [(a `withSize` b), b]
 para sz = vcatA left . map (text sz)
 
-mkdiagram :: ([String],Frame) -> (DT.Point, DT.Diagram)
-mkdiagram (str,((x,y):_)) = ((fromIntegral x,fromIntegral y),para 12 str)
+plainbubble :: [String] -> Diagram
+plainbubble strs = bubble `underlay` writing
+ where bubble w h = fc white $ lc white $ roundRect (w*1.2) (h*1.2)
+       writing = para 12 strs
+
+entail :: DT.Point -> DT.Point -> Diagram -> Diagram
+entail (fx,fy) (bx,by) = atop tail
+ where tail w h = fc white $ lc white $ curved 0 $ pathFromVertices $ map transform $ map (\(x,y) -> (x*w/3, y*h/2)) pts
+       transform = (if bx>fx then negate else id) *** (if by>fy then negate else id)
+       pts = [(-0.25,0),(-1,-1),(0.25,0)]
+
+mkdiagram :: ([String],Frame,Dim) -> (DT.Point, DT.Diagram)
+mkdiagram (str,((x,y):_),(x',y')) =
+  (bb, entail fr bb (plainbubble str))
+   where fr@(fx,fy) = (fromIntegral x', fromIntegral y')
+         bb@(bx,by) = (fromIntegral x, fromIntegral y)
 
 -- renderOverlayPNG <- srcfile dstfile diagram
 writePix dir pix = renderOverPNG (dir `combine` bgImage pix) fn d
