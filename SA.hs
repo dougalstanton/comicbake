77d5305eb5ad2af048cd9d56e091a62b6942602e
Dougal Stanton <dougal@dougalstanton.net>
2009-10-27 22:06:15 +0000

    Clean up SA module, remove compiler warnings
    Ignore-this: f47c9d14587f6e4ef2f9a4ec0601e85
    
    darcs-hash:20091027220615-1b04a-4479bc93f498f35fae9c94eaa958fc2f4d4062fe

--- SA.hs
+++ SA.hs
@@ -26,18 +26,21 @@ anneal cfg st = go temps (randoms cfg) start st
        start = (state st, (value st) (state st))
 
 
+-- The temperatures and the random values should be infinite lists
 go :: [Temperature] -> [Double] -> (a,Energy) -> State a -> a
-go (t:ts) (r:rs) (beststate,bestscore)
-   st@(State cur energy mknew) | t < epsilon = beststate
-                               | otherwise   = st'
-  where st' = if delta > 0 || exp(delta/t) > r
-                 then go ts rs newbest (update st next)
-		 else go (t:ts) rs newbest st
-	delta = energy next - energy cur
+go (t:ts) (r:rs) (bestval,bestscore) oldst@(State cur cost mknew)
+                    | t < epsilon = bestval
+                    | otherwise   = go newtemps rs newbest newst
+  where usenew = delta > 0 || exp(delta/t) > r
+	newtemps = if usenew then ts else (t:ts)
+	newst = if usenew then update oldst next else oldst
+	newbest  = if cost next < bestscore
+	                     then (next,cost next)
+		             else (bestval,bestscore)
+
+	delta = cost cur - cost next
 	next  = mknew r cur
-	newbest = if energy next < bestscore
-	             then (next,energy next)
-		     else (beststate,bestscore)
+go _ _ _ _ = error "SA failed: reached end of infinite list!"
 
 update :: State a -> a -> State a
 update st new = st { state = new }
