6fdd0a6e6e4023aa488c72a53a2c278236a9a1ec
Dougal Stanton <dougal@dougalstanton.net>
2010-10-18 12:41:27 +0100

    Rename Layout module to Objects, reduce responsibility
    Ignore-this: d1542fc22221cfb10ee8b7a2ba29095d
    
    darcs-hash:20101018114127-1b04a-1b77aad508fdd2fe4e4a7747442205cb68b2aabb

---
+++ Objects.hs
@@ -0,0 +1,27 @@
+module Objects where
+
+import Data.List
+
+import Script
+import Parse
+import Sanitise
+
+-- Collect all the individual objects which are mentioned in the
+-- script but don't assign them any location yet. They are like
+-- cut-out bits of paper: we glue them all together in the next
+-- stage.
+
+data Text = Bubble [String] Character deriving (Eq,Show)
+data Person = Person Character deriving (Eq,Show)
+data Objects = Objs [Text] [Person] deriving (Eq,Show)
+
+action2text :: Action -> Text
+action2text a = Bubble (speech a) (character a)
+
+allcharacters :: [Action] -> [Character]
+allcharacters = nub . map character
+
+gatherobjects :: [Action] -> Objects
+gatherobjects input = Objs dialogue people
+  where people = map Person $ allcharacters input
+        dialogue = map action2text input
