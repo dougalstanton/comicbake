b3c00cff4ad12437ad4563bf3031e53001472515
Dougal Stanton <dougal@dougalstanton.net>
2010-11-01 10:54:26 +0000

    Pass in context directory for loading images, image maps
    Ignore-this: d96a517e46fc15ea65bc7164ffbfe337
    
    darcs-hash:20101101105426-1b04a-abf7864bf2edd2fbe5f0ddc764cc0b104ee7a9e6

--- Transform.hs
+++ Transform.hs
@@ -14,22 +14,22 @@ import Layout
 import CastList
 import ImageMapParse
 
-processScene :: Scene -> IO (Panel [Speech])
-processScene scene = do
+processScene :: FilePath -> Scene -> IO (Panel [Speech])
+processScene dir scene = do
   let panelobjs = fmap gatherobjects $ sanitiseScene scene
-  panelattrtext <- loadCastList panelobjs
-  bgImageSize panelattrtext
+  panelattrtext <- loadCastList dir panelobjs
+  bgImageSize dir panelattrtext
 
 -- Load and parse the cast list from the .map file.
-loadCastList :: Panel Objects -> IO (Panel [AttributedText])
-loadCastList panel = do
-  let imagemapfile = background panel `replaceExtension` "map"
+loadCastList :: FilePath -> Panel Objects -> IO (Panel [AttributedText])
+loadCastList dir panel = do
+  let imagemapfile = dir </> background panel `replaceExtension` "map"
   castlist <- parseImageMap `fmap` readFile imagemapfile
   return $ fmap (convert castlist) panel
 
 -- Load the background image to get its dimensions.
 -- TODO: Store the image data rather than fetching it twice.
-bgImageSize :: Panel [AttributedText] -> IO (Panel [Speech])
-bgImageSize panel = do
-  dims <- (loadPngFile (background panel) >>= imageSize)
+bgImageSize :: FilePath -> Panel [AttributedText] -> IO (Panel [Speech])
+bgImageSize dir panel = do
+  dims <- (loadPngFile (dir </> background panel) >>= imageSize)
   return $ fmap (fixspeeches dims) panel
