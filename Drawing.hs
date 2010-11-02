909efd127ae2ce3605cabc278e24f756922d948e
Dougal Stanton <dougal@dougalstanton.net>
2010-11-02 18:38:29 +0000

    Clean up drawings, make nicer rectangle borders
    Ignore-this: 1717219d188b8658d5ac3cb4ce06d2a6
    Text is better placed in better-proportioned bubbles with cute grey borders and there are lots more type signatures.
    
    darcs-hash:20101102183829-1b04a-1a230b69f51ac55559f6d201db30b3a056d2801f

--- Drawing.hs
+++ Drawing.hs
@@ -11,15 +11,19 @@ fontsize = 10
 
 -- Common colours.
 white = rgb 255 255 255
+grey  = rgb 200 200 200
 black = rgb 0 0 0
 
 -- Useful functions
 half n = div n 2
 quarter = half . half
+pair f (a,b) = (f a,f b)
+scale f = round . f . fromIntegral
+scaleXY x y (a,b) = (scale (*x) a, scale (*y) b)
 
 -- Freetype bounds not very reliable
 -- coordinates in order: bl br tr tl
-boundsize (a,b,c,d) = (6 + maxx - minx, 6 + maxy - miny)
+boundsize (a,b,c,d) = (maxx - minx, maxy - miny)
   where bounds = [a,b,c,d]
         maxx = maximum $ map fst bounds
         minx = minimum $ map fst bounds
@@ -27,20 +31,30 @@ boundsize (a,b,c,d) = (6 + maxx - minx, 6 + maxy - miny)
         miny = minimum $ map snd bounds
 
 -- Draw wide oval around a rectangle of the given width and height.
-oval (w,h) pt = \img -> do
-  drawFilledEllipse pt sz white img
-  where sz = (adj w, adj h)
-        adj n = round (2 * fromIntegral n / sqrt 2)
+oval :: Size -> Point -> Image -> IO ()
+oval sz pt = \img -> do
+  drawFilledEllipse pt (pair adjust sz) white img
+  where adjust = scale (\x -> 2 * x / sqrt 2)
 
--- Draw rectangle of the given width and height.
-rect (w,h) (x,y) = drawFilledRectangle pt1 pt2 white
+-- Draw a box with border around this point
+frame :: Size -> Point -> Image -> IO ()
+frame (w,h) pt = \img -> do
+  rect (w+2,h+2) pt grey img
+  rect (w,h) pt white img
+
+-- Draw rectangle of the given width and height centred on (x,y)
+rect :: Size -> Point -> Color -> Image -> IO ()
+rect (w,h) (x,y) colour = drawFilledRectangle pt1 pt2 colour
   where pt1 = (x - half w, y - half h)
         pt2 = (x + half w, y + half h)
 
 -- Very handy for debugging on the final image.
-dot (x,y) = drawFilledRectangle (x,y) (succ x,succ y) (rgb 255 0 0)
+dotR,dotB :: Point -> Image -> IO ()
+dotR (x,y) = drawFilledRectangle (x,y) (succ x,succ y) (rgb 255 0 0)
+dotB (x,y) = drawFilledRectangle (x,y) (succ x,succ y) (rgb 0 0 255)
 
 -- Draw the supplied string list centred on this position.
+text :: String -> [String] -> Point -> Image -> IO Point
 text name strs (x,y) = \img -> do
   let str = init $ unlines strs -- drop that last \n
   -- drawString takes the lower-left co-ordinate of the first letter
@@ -50,16 +64,19 @@ text name strs (x,y) = \img -> do
   bounds <- measureString name fontsize 0 (x,y) str black
   let (_,fontheight) = boundsize firstline
       (w,h) = boundsize bounds
-      pt = (x - half w, fontheight + y - half h)
+      fudge = scale (*0.7) fontheight
+      pt = (x - half w, fudge + y - half h)
   drawString name fontsize 0 pt str black img
   return (w,h)
 
 -- Draw a tail which curves from the start point in the general
 -- direction of the destination.
+curvetail :: Size -> Point -> Point -> Image -> IO ()
 curvetail sz pt dst = drawArc pt sz start end white
   where start = 0; end = const 45 dst
 
 -- Draw smaller blobs between the start and end points.
+blobtail :: Size -> Point -> Point -> Image -> IO ()
 blobtail (width,height) (sx,sy) (dx,dy) img =
   mapM_ (\(sz,pt,_) -> oval sz pt img) $ take 3 $ iterate smaller firstblob
   where (vecx,vecy) = ((dx-sx)`div`3,(dy-sy)`div`3)
@@ -69,6 +86,7 @@ blobtail (width,height) (sx,sy) (dx,dy) img =
                      (sx+vecx,sy+vecy),(half  vecx, half vecy))
 
 -- Create a thought bubble with a trail of smaller bubbles.
+thoughtbubble :: [String] -> Point -> Point -> Image -> IO ()
 thoughtbubble strs pt dst img = do
   useFontConfig True
   -- We need to scale the bubbles around the text, so first
@@ -80,10 +98,11 @@ thoughtbubble strs pt dst img = do
   return ()
 
 -- Create a speech bubble with a tail pointing at the speaker.
+speechbubble :: [String] -> Point -> Point -> Image -> IO ()
 speechbubble strs pt dst img = do
   useFontConfig True
   dims <- text "Sans" strs pt img
-  rect dims pt img
+  frame (scaleXY 1.2 1.5 dims) pt img
   -- TODO: add in tail pointing at speaker
   text "Sans" strs pt img
   return ()
