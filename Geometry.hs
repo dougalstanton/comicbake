a40bb9467ef0689571322756a8e3871ec92315b3
Dougal Stanton <dougal@dougalstanton.net>
2010-11-02 18:28:18 +0000

    Make overlap testing more inclusive
    Ignore-this: e79c53580a2644af957898ae1a637e39
    
    darcs-hash:20101102182818-1b04a-d1b6bf3273c8da99c795354bfbdec3e5f4824221

--- Geometry.hs
+++ Geometry.hs
@@ -5,6 +5,6 @@ import Script
 overlaps b1 b2 = overlapX b1 b2 && overlapY b1 b2
 overlapRange (a1,a2) (b1,b2) = straddle || inside
   where straddle = b1 < a1 && a1 < b2 || b1 < a2 && a2 < b2
-        inside = a1 < b1 && b2 < a2 || b1 < a1 && a2 < b2
+        inside = a1 <= b1 && b2 <= a2 || b1 <= a1 && a2 <= b2
 overlapX (Box (a1,_) (a2,_)) (Box (b1,_) (b2,_)) = overlapRange (a1,a2) (b1,b2)
 overlapY (Box (_,a1) (_,a2)) (Box (_,b1) (_,b2)) = overlapRange (a1,a2) (b1,b2)
