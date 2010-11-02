d99ce4344f0d7fda592c61d90a3af6fbddb03ad6
Dougal Stanton <dougal@dougalstanton.net>
2010-11-02 18:29:19 +0000

    Put black margins around outside of each panel
    Ignore-this: fd7eccebac7284c5f9af3922cb8a29b5
    
    darcs-hash:20101102182919-1b04a-79c981846c9e1617936715bf3ab3bd6f372e286a

--- Pix.hs
+++ Pix.hs
@@ -10,14 +10,23 @@ import Locations
 import Layout
 import Drawing
 
-addspeech img (Speech (Loc txtbox txt) (Loc speakerbox _)) = do
-  speechbubble txt (midpoint txtbox) (midpoint speakerbox) img
+addspeech off img (Speech (Loc txtbox txt) (Loc speakerbox _)) = do
+  speechbubble txt (off += midpoint txtbox) (off += midpoint speakerbox) img
   return img
+  where (+=) v (x,y) = (v+x,v+y)
 
 writeImage :: FilePath -> FilePath -> FilePath -> Panel [Speech] -> IO (Panel FilePath)
 writeImage srcdir destdir filename panel = do
-  img <- loadPngFile (srcdir </> (background panel))
-  foldM addspeech img (action panel)
+  img <- expandImage =<< loadPngFile (srcdir </> (background panel))
+  foldM (addspeech 10) img (action panel)
   let imgfile = destdir </> filename <.> show (number panel) <.> "png"
   savePngFile imgfile img
   return (panel {action = imgfile})
+
+expandImage :: Image -> IO Image
+expandImage src = do
+  (width,height) <- imageSize src
+  dst <- newImage (width+20,height+20)
+  fillImage (rgb 0 0 0) dst
+  copyRegion (0,0) (width,height) src (10,10) dst
+  return dst
