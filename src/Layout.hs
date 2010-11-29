29fe1dc73b8516b06b3a138d7ce218ff38ba3cfd
Dougal Stanton <dougal@dougalstanton.net>
2010-11-29 17:29:58 +0000

    Use estimated size to determine bubble placement
    Ignore-this: c114bfd3ebd7cb20d5e4f3834e71ef3
    Use the full estimated Box rather than the midpoint, so we can avoid overlapping boxes etc.
    
    darcs-hash:20101129172958-1b04a-a759cac049205bc09443373ab16b8be66ce4e1a4

--- src/Layout.hs
+++ src/Layout.hs
@@ -48,13 +48,15 @@ joinarms arm1 arm2 pt = zipWith f (arm1 pt) (arm2 pt)
   where f (x,_) (_,y) = (x,y)
 
 -- Only consider points that are within the bounds of the original image.
-inbounds (w,h) (x,y) = x >= 0 && x <= w && y >= 0 && y <= h
+inbounds :: Dim -> Box -> Bool
+inbounds (w,h) box = x >= 0 && x <= w && y >= 0 && y <= h
+  where (x,y) = midpoint box
 
 -- Sometimes it looks nice if a bubble spills over the edge a bit but
 -- we don't want it to disappear completely.
 -- TODO: Fine-tune how much margin we want to allow.
-onscreen :: Dim -> [String] -> Pt -> Bool
-onscreen (w,h) txt pt = go (-20,-20) (w+20,h+20) (txt2box pt txt)
+onscreen :: Dim -> Box -> Bool
+onscreen (w,h) box = go (-20,-20) (w+20,h+20) box
   where go (left,top) (right,bottom) (Box (xl,yt) (xr,yb)) =
            and $ [xl>=left,yt>=top,xr<=right,yb<=bottom]
 
@@ -67,17 +69,18 @@ onscreen (w,h) txt pt = go (-20,-20) (w+20,h+20) (txt2box pt txt)
 -- TODO: Change arm order to top-bottom for vertical component
 -- of diagonals. Conversations should favour starting higher
 -- and moving down later.
-type Arm = [Pt]
-rankarms :: [String] -> Dim -> Pt -> [Arm]
-rankarms txt sz@(pw,ph) pt@(x,y) = map keep candidates
-  where keep = filter (onscreen sz txt) . takeWhile (inbounds sz)
+type Arm = [FixedText]
+rankarms :: Dim -> [String] -> FixedPerson -> [Arm]
+rankarms sz@(pw,ph) txt person = map keep candidates
+  where keep = filter (onscreen sz . location) . takeWhile (inbounds sz . location)
+        pt@(x,y) = midpoint person -- location of speaker
         hdir = x > (pw `div` 2) -- left?
         vdir = y > (ph `div` 2) -- up?
         diags = [ joinarms (horiz hdir)       (vertic vdir)
                 , joinarms (horiz hdir)       (vertic (not vdir))
                 , joinarms (horiz (not hdir)) (vertic vdir)]
         flats = [vertic vdir, horiz hdir, vertic (not vdir)]
-        candidates = map ($pt) (diags ++ flats)
+        candidates = map (map (locatetext txt) . (\f -> f pt)) (diags ++ flats)
 
 -- Process the text bubbles, testing each candidate location for
 -- suitability and discarding the undesirable ones. There will
@@ -91,17 +94,15 @@ txt2box (x,y) txt = Box (x-w, y-h) (x+w, y+h)
 
 -- A point is unsuitable if the resulting box would overlap any
 -- known boxes (typically, characters).
-alreadyused :: [Box] -> [String] -> Pt -> Bool
-alreadyused occupied txt pt = any (overlaps txtbox) occupied
-  where txtbox = txt2box pt txt
+alreadyused :: [Box] -> Box -> Bool
+alreadyused occupied box = any (overlaps box) occupied
 
 -- A point is unsuitable if it appears "before" (in traditional
 -- comic reading order) another point which is already fixed.
 -- TODO: Use bounding boxes instead of centre points. Upper bound
 -- should be below the lowest bound of all previous points.
-outoforder :: [Pt] -> Pt -> Bool
-outoforder previous this = any (this `before`) previous
-  where before (x1,y1) (x2,y2) = y1 < y2 || abs (y2 - y1) < 20
+inorder :: [FixedText] -> Box -> Bool
+inorder previous current = all (current >) (map location previous)
 
 -- Given an arm we move along its length until we find a position
 -- that isn't *un*suitable and stop there, but we keep hold of
@@ -109,35 +110,37 @@ outoforder previous this = any (this `before`) previous
 -- If we can't find a suitable candidate then we return Nothing
 -- and hope that other elements can be moved around to make room
 -- for this one (ie backtracking).
-findcandidate :: [Box] -> [String] -> [Pt] -> [Arm] -> Maybe (Pt,[Arm])
-findcandidate _ _ _ [] = Nothing
-findcandidate boxs txt prev (arm:arms) =
+findcandidate :: [Box]       -- ^ Fixed character locations
+              -> [FixedText] -- ^ Provisional placement for other bubbles
+              -> [Arm]       -- ^ Candidate places for this bubble
+              -> Maybe (FixedText,[Arm])
+findcandidate _ _ [] = Nothing
+findcandidate boxs prev (arm:arms) =
   case dropWhile unsuitable arm of
-       []     -> findcandidate boxs txt prev arms
+       []     -> findcandidate boxs prev arms
        [c]    -> Just (c,arms)
        (c:cs) -> Just (c,cs:arms)
-  where unsuitable pt = alreadyused boxs txt pt || outoforder prev pt
+  where unsuitable (Loc box _) = alreadyused boxs box || not (inorder prev box)
 
-type Path = ([String],[Arm])
+type Path = [Arm]
 
 -- Depth first search to find the first acceptable list of
 -- locations for all text elements.
-dfs :: [Box] -> [Path] -> [Path] -> [Pt] -> [Pt]
+dfs :: [Box] -> [Path] -> [Path] -> [FixedText] -> [FixedText]
 dfs _    []   _    done = done
 dfs boxs todo alts done =
-  let (txt,arms):rest = todo
-  in case findcandidate boxs txt done arms of
-       Just (pt,remains) -> dfs boxs rest ((txt,remains):alts) (pt:done)
+  let arms:rest = todo
+  in case findcandidate boxs done arms of
+       Just (chosen,remains) -> dfs boxs rest (remains:alts) (chosen:done)
        Nothing -> if null done || null alts -- should be same thing
                   then error "Layout:dfs couldn't place anything"
                   else dfs boxs ((head alts):todo) (tail alts) (tail done)
 
-candidates :: [Box] -> [Path] -> [Pt]
-candidates occupied potentials = dfs occupied potentials [] []
+candidates :: [FixedPerson] -> [Path] -> [FixedText]
+candidates occupied potentials = dfs (map location occupied) potentials [] []
 
-mkpaths :: Dim -> [[String]] -> [Box] -> [Path]
-mkpaths sz txts speakers = map mkpath (zip txts speakers)
- where mkpath (txt,speaker) = (txt,rankarms txt sz (midpoint speaker))
+mkpaths :: Dim -> [[String]] -> [FixedPerson] -> [Path]
+mkpaths sz txts speakers = zipWith (rankarms sz) txts speakers
 
 -- A single speech item is the text with its location attached
 -- to a speaker, with their location.
@@ -145,18 +148,13 @@ type FixedText = Located [String]
 data Speech = Speech FixedText FixedPerson deriving (Eq,Show)
 
 locatetext :: [String] -> Pt -> FixedText
-locatetext txt (x,y) = Loc (Box (x - w', y - h') (x + w', y + h')) txt
-  where (w,h) = estimate txt
-        w' = w `div` 2
-        h' = h `div` 2
+locatetext txt pt = Loc (txt2box pt txt) txt
 
 -- Given the size of the background image and a list of texts with
 -- known speaker locations, we return a list which identifies the
 -- locations of speakers and their speeches.
 fixspeeches :: Dim -> [AttributedText] -> [Speech]
 fixspeeches sz attribtexts = zipWith Speech fixedtexts fixedpersons
-  where positions = reverse $ candidates occupied $ mkpaths sz txts occupied
-        fixedtexts = zipWith locatetext txts positions
+  where fixedtexts = reverse $ candidates fixedpersons $ mkpaths sz txts fixedpersons
         (txts,fixedpersons) = unzip $ map unattrib attribtexts
-        occupied = map (\(Loc position _) -> position) fixedpersons
         unattrib (AttrBubble s fp) = (s,fp)
