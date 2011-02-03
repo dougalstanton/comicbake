d6d1d3ade47efface50497e9a17ab7f2cdb9fcae
Dougal Stanton <dougal@dougalstanton.net>
2011-02-03 18:27:43 +0000

    Rewrite and simplify script parser
    Ignore-this: d05dd4a2899d2072be44b511eed27645
    Totally rewrote the parser to make better use of Parsec, provide better error messages and take fewer lines to do it in. The file format is simpler to use and slightly more flexible to write for script authors too.
    
    darcs-hash:20110203182743-1b04a-e547a068701cee6839ae5ab5e5488d2802f9887f

--- src/Parsing/ScriptParse.hs
+++ src/Parsing/ScriptParse.hs
@@ -2,140 +2,116 @@ module Parsing.ScriptParse
        (Scene (..), Action (..)
        ,parseScript, parseScriptFromFile) where
 
-import Text.ParserCombinators.Parsec hiding (space, spaces)
-import Text.ParserCombinators.Parsec.Error
+import Control.Arrow (left)
+import Control.Monad (mplus)
+import Text.ParserCombinators.Parsec
 import Data.Maybe
-import Data.List.Split (split, dropInitBlank, keepDelimsL, whenElt)
-import Data.List (mapAccumL)
+import Data.Char (isSpace)
 
-import Script hiding (Panel(..))
-import qualified Script as S
-
-data Scene = Scene
-    { sceneNumber     :: Int
-    , sceneBackground :: FilePath
-    , sceneAction     :: [Action]
-    } deriving (Eq, Show)
+import Script
 
 data Action = Action
     { character :: Character
+    , thinking  :: Bool
     , speech    :: [String]
     } deriving (Eq, Show)
 
+data ComicState = S { savedUrl :: Maybe FilePath }
+emptyState = S Nothing
+saveUrl url = updateState (\s -> s {savedUrl = url})
+
+ifIndented yes no = do pos <- getPosition
+                       if (sourceColumn pos > 1) then yes else no
+
+type ScriptParser a = GenParser Char ComicState a
+
+parseScriptFromFile :: FilePath -> IO (Either String (Script [Panel [Action]]))
 parseScriptFromFile fp = parseScript fp `fmap` readFile fp
 
-parseScript :: FilePath -> String -> Either String (Script [S.Panel [Action]])
-parseScript fp str = if null errs
-                     then Right $ convert fp $ fromOk res
-                     else Left $ unlines errs
-  where res = runParser document [] fp str
-        -- create proper error messages
-        errs = either (map messageString . errorMessages) problems res
-        fromOk (Right a) = a
-
-sanitiseScene :: Scene -> S.Panel [Action]
-sanitiseScene sc = S.Panel
-    { S.number = sceneNumber sc
-    , S.background = sceneBackground sc
-    , S.bgsize = Nothing
-    , S.action = sceneAction sc }
-
-convert :: FilePath -> [S] -> Script [S.Panel [Action]]
-convert fp ss = Script t [a,d] fp (map (sanitiseScene . mkScene) scenes)
- where (Preamble t s d a ms) = head ss
-       scenes = splitRun isStart $ tail ss
-       splitRun p = split (dropInitBlank $ keepDelimsL $ whenElt p)
-
-mkScene :: [S] -> Scene
-mkScene = foldr f z
-  where z = Scene 0 "" []
-        f (Start n url) s = s { sceneNumber = n, sceneBackground = url }
-        f (Content c d) s = s { sceneAction = newAction c d s }
-        newAction c d s@(Scene _ _ a) = Action c d : a
-
-problems :: [S] -> [String]
-problems ss = mapMaybe ($ss) ps
-  where ps = [onePreamble,allBackgrounds]
-
-onePreamble :: [S] -> Maybe String
-onePreamble ss = if ps then Nothing else Just errmsg
-  where ps = all isPreamble $ take 1 ss
-        errmsg = "The beginning of the script must contain a preamble"
-
-allBackgrounds :: [S] -> Maybe String
-allBackgrounds ss | length s < 1  = Just "You need at least one scene."
-                  | noBg (head s) = Just "First scene needs a bg image."
-		  | otherwise     = Nothing
-  where s = filter isStart ss
-	noBg (Start _ bg) = null bg
-
-isPreamble (Preamble _ _ _ _ _) = True
-isPreamble _                    = False
-isStart    (Start _ _)          = True
-isStart    _                    = False
-isContent  (Content _ _)        = True
-isContent  _                    = False
-
--- We use this type as a temporary measure for parsing
--- the data in the script, before sanity checking.
-data S = Preamble String String String String [String]
-       | Start Int String
-       | Content String [String]
-	deriving (Eq, Show)
+parseScript :: FilePath -> String -> Either String (Script [Panel [Action]])
+parseScript fp str = left show $ runParser scriptParser emptyState fp str
 
 --
 -- Below here is the base level parser using Parsec.
 --
 
-type ScriptParser a = GenParser Char FilePath a
 
-space = char ' '
-spaces = many1 space
-newlines = many newline
-newlines1 = many1 newline
+scriptParser :: ScriptParser (Script [Panel [Action]])
+scriptParser = do bare   <- preambleParser
+                  scenes <- manyTill sceneParser eof
+                  return $ bare { scriptContents = scenes }
 
-document :: ScriptParser [S]
-document = do p <- preamble <?> "preamble"
-	      s <- script
-              return (p:s)
+preambleParser :: ScriptParser (Script [Panel [Action]])
+preambleParser = do metadata <- catMaybes `fmap` many comment
+                    let title  = fetch "title" metadata
+                        author = fetch "author" metadata
+                    file <- (sourceName . statePos) `fmap` getParserState
+                    many (space <|> newline)
+                    return $ Script title [author] file []
+    where fetch key = fromMaybe ("Unknown "++key) . lookup key
 
--- decide on data requirements and use count or permute function
-preamble = do title  <- commentline
-	      series <- commentline
-              date   <- commentline
-              author <- commentline
-	      misc   <- many (commentline)
-	      newlines
-  	      return $ Preamble title series date author misc
- where commentline = string "--" >> spaces >> manyTill anyChar newlines1
+sceneParser :: ScriptParser (Panel [Action])
+sceneParser = do (num,bgfile) <- sceneHeaderParser
+                 speeches     <- speechListParser
+                 return $ Panel num bgfile Nothing speeches
 
-scenekw = string "Scene" <?> "scene header"
+sceneHeaderParser :: ScriptParser (Int,FilePath)
+sceneHeaderParser = do sceneKeyword >> spaces
+                       num <- many1 digit <?> "scene number"
+                       char '.' >> spaces
+                       url <- urlParser <?> "path to background image"
+                       many (space <|> newline)
+                       return (read num, url)
+    where urlParser = do oldUrl <- savedUrl `fmap` getState
+                         newUrl <- optionMaybe filename
+                         let url = newUrl `mplus` oldUrl
+                         maybe pzero (\u -> saveUrl url >> return u) url
 
-script = manyTill (sceneheader <|> action) eof
+sceneKeyword :: ScriptParser String
+sceneKeyword = string "Scene" <?> "Scene header"
 
--- todo: find a proper legal filename characters
-filename = between (char '<') (char '>') legalchars
-  where legalchars = many (alphaNum <|> char '.' <|> char '/')
+speechListParser :: ScriptParser [Action]
+speechListParser = do { eof; return [] }
+                   <|> do { lookAhead sceneKeyword ; return [] }
+                   <|> do speech <- speechParser
+                          others <- speechListParser
+                          return (speech:others)
 
--- If no filename is given we fill in the most recently
--- used one. Then we store the current filename for later.
-sceneheader = do try scenekw ; spaces
-                 num <- many1 digit ; char '.' ; many space
-		 url <- optfile
-                 setState url
-		 newlines
-		 return $ Start (read num) url
-  where optfile = do oldurl <- getState
-                     if null oldurl
-                        then filename <?> "file path"
-                        else option oldurl filename
+speechParser :: ScriptParser Action
+speechParser = do speaker <- speakerParser <?> "speaker's name"
+                  thought <- try modeParser <?> "optional thought marker"
+                  speech  <- textParser <?> "speech"
+                  return $ Action speaker thought speech
 
--- First character of name must be a letter
-action = do name <- realname ; char ':'
-	    speech <- many (spaces >> legalchar `manyTill` newline)
-	    newlines
-            return $ Content name speech
-  where legalchar = noneOf "\n\r"
-        realname = do c <- letter <?> "character name"
-                      cs <- many (letter <|> space)
-                      return (c:cs)
+speakerParser :: ScriptParser String
+speakerParser = do name <- manyTill anyChar (char ':')
+                   spaces
+                   return (trim name)
+  where trim = dropWhile isSpace . reverse . dropWhile isSpace . reverse
+
+modeParser :: ScriptParser Bool
+modeParser = do mode <- optionMaybe (string "(Thinking)" >> spaces >> newline)
+                return (isJust mode)
+
+textParser :: ScriptParser [String]
+textParser = spaces >> ifIndented lineParser (return [])
+  where lineParser = do l  <- anyChar `manyTill` newline
+                        ls <- textParser
+                        return (l:ls)
+
+filename :: ScriptParser FilePath
+filename = between (char '<') (char '>') (many (noneOf "\n\r>"))
+
+comment :: ScriptParser (Maybe (String,String))
+comment = do delimiter
+             mkv <- optionMaybe (try keyvalue)
+             optional (manyTill anyChar newline)
+             many newline
+             return mkv
+  where delimiter = string "--" >> spaces
+
+keyvalue :: ScriptParser (String,String)
+keyvalue = do key <- many1 letter
+              char ':' >> spaces
+              val <- manyTill anyChar newline
+              return (key,val)
