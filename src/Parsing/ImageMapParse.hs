6edc0caa95784ba2d72fe3bf174e9922cb18c6a1
Dougal Stanton <dougal@dougalstanton.net>
2013-04-01 00:33:32 +0100

    fiddling with image map parsing

--- src/Parsing/ImageMapParse.hs
+++ src/Parsing/ImageMapParse.hs
@@ -8,8 +8,7 @@ parseImageMap :: String -> CastList
 parseImageMap = foldr (uncurry addCharacter) emptyCast . map getData . keepAreaTag . parseTags
     where keepAreaTag = filter (isTagOpenName "area")
           getData t = (fromAttrib "href" t, mkCoordPairs $ readCoords $ fromAttrib "coords" t)
-          readCoords str = read ('[':str ++ "]")
+          readCoords = map read . words . map (\c -> if c==',' then ' ' else c)
           mkCoordPairs :: [Int] -> [(Int,Int)]
-          mkCoordPairs [] = []
-          mkCoordPairs [_] = []
           mkCoordPairs (a:b:ls) = (a,b):mkCoordPairs ls
+          mkCoordPairs _        = []
